95e6d4e9df576e4a38115596a64e94fee952be61
DieWay0123 <a0987974634@gmail.com>
2025-05-06 01:49:16 +0800

    3rd

--- client_GUI.py
+++ client_GUI.py
@@ -7,8 +7,8 @@ class ClientGUI:
     def __init__(self, root):
         # socket相關參數
         self.socket = None
-        self.sever_address = None
-        self.client_address = ('10.223.12.187', 12345)
+        self.server_address = None
+        self.client_address = ('127.0.0.1', 12345)
         self.receive_thread = None
 
         # 猜數字相關參數
@@ -18,9 +18,15 @@ class ClientGUI:
         self.guess_count = 0
         self.start_time = None
         
+        # timer相關參數
+        self.timeout_timer = None # 用以存放timer thread
+        self.TIMEOUT_DURATION = 120 # 設定多久沒有動作會timeout(單位秒)
+        self.game_running = False
+        
+        # Tkinter相關參數
         self.root = root
         self.root.title("UDP猜字串-Client")
-        self.root.geometry("900x540")
+        self.root.minsize(900, 540)
 
 
         # === 輸入連線相關資訊部分GUI ===
@@ -48,6 +54,10 @@ class ClientGUI:
         # 用於監控的訊息輸出框
         self.output_text = tk.Text(self.text_frame, state="disabled")
         self.output_text.grid(row=0, column=0, sticky="nsew")
+        self.output_text.tag_config("bold", font=("Helvetica", 10, "bold"))
+        self.output_text.tag_config("error", foreground="red")
+        self.output_text.tag_config("success", foreground="green")
+        self.output_text.tag_config("info", foreground="blue", font=("Helvetica", 10, "bold"))
         # 加上scrollbar，可以調整訊息框閱讀範圍
         scrollbar_x = tk.Scrollbar(self.text_frame, orient="horizontal", command=self.output_text.xview)
         scrollbar_y = tk.Scrollbar(self.text_frame, orient='vertical', command=self.output_text.yview)
@@ -76,11 +86,28 @@ class ClientGUI:
         self.guess_frame.grid(row=2, column=0)
 
     def replay_game(self):
+        self.reset_timeout_timer()
         if self.socket:
             self.socket.sendto(f"[Replay]:{self.client_address}".encode(), self.server_address)
             self.replay_button.config(state="disabled")
             self.guess_entry.delete(0, tk.END)
-            self.modify_output_text("[Replay Requesting]已請求重新開始，請稍候Server設定新答案...\n")
+            self.modify_output_text("[Replay Requesting]已請求重新開始，請稍候Server設定新答案...\n", "info")
+
+    def test_server_connection(self, server_address):
+        ip = server_address[0]
+        port = server_address[1]
+        try:
+            self.socket.sendto(f"[Connecting]: client->{ip}:{port}".encode(), self.server_address)
+            data, addr = self.socket.recvfrom(1024)
+            self.modify_output_text(f"[Connection Success!]: 伺服器回應為: {data.decode()}\n", "success")
+            return True
+        except socket.timeout:
+            return False
+        except Exception as e:
+            self.modify_output_text(f"[Error]: {e}\n", "error")
+            return False
+        finally:
+            self.socket.settimeout(None)
 
     def start_game(self):
         ip = self.ip_entry.get()
@@ -88,32 +115,42 @@ class ClientGUI:
         username = self.name_entry.get()
         
         if not ip or not port or not username:
-            self.modify_output_text("[Error]: 請填寫完整IP、Port和username\n")
+            self.modify_output_text("[Error]: 請填寫完整IP、Port和username\n", "error")
             return
         
         try:
             port = int(port)
         except ValueError:
-            self.modify_output_text("[Error]: 請確認填寫之port為整數數字")
+            self.modify_output_text("[Error]: 請確認填寫之port為整數數字\n", "error")
             return
 
         self.server_address = (ip, port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            self.socket.bind(self.client_address)
-        except Exception as e:
-            print(e)
-        self.modify_output_text(f"[Success]: 已連線到伺服器{ip}:{port}，等待server設定答案...\n")
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+        self.socket.bind(self.client_address)
+        self.socket.settimeout(10)
         
+        # ================= 確認server啟動 =======================
+        if not self.test_server_connection(self.server_address):
+            self.modify_output_text("[Error]: 連線失敗，無法連線到伺服器，請確認伺服器是否已啟動\n", "error")
+            self.socket.close()
+            return
+        
+        # =======================================================
+
+        self.modify_output_text(f"[Success]: 已連線到伺服器{ip}:{port}，等待server設定答案...\n", "success")
+        self.game_running = True
         # 告訴server client的地址
-        self.socket.sendto(f"[Connecting]: client->{ip}:{port}".encode(), self.server_address)
         self.guess_count = 0
         self.username = username
+        self.start_button.config(state="disabled")
         self.receive_thread = threading.Thread(target=self.receive_response, daemon=True)
         self.receive_thread.start()
+        self.reset_timeout_timer()
     
     def send_guess(self):
         guess = self.guess_entry.get().strip().upper()
+        self.reset_timeout_timer()
 
         # 若目前server還未設定好答案就送出猜測，則跳出等待訊息        
         if not self.ready_to_guess:
@@ -122,58 +159,111 @@ class ClientGUI:
         
         # 驗證猜測輸入部分
         if any(c not in "0123456789ABCDEF" for c in guess):
-            self.modify_output_text("[Error]: 請只輸入0-0或A-F的字元\n")
+            self.modify_output_text("[Error]: 請只輸入0-0或A-F的字元\n", "error")
             return
         if len(set(guess)) != len(guess):
-            self.modify_output_text("[Error]: 請確認輸入字元皆不重複\n")
+            self.modify_output_text("[Error]: 請確認輸入字元皆不重複\n", "error")
             return
         if self.answer_length != 0 and len(guess) != self.answer_length:
-            self.modify_output_text(f"[Error]: 請確認輸入長度為{self.answer_length}\n")
+            self.modify_output_text(f"[Error]: 請確認輸入長度為{self.answer_length}\n", "error")
             return
         
         # 驗證輸入正確後，開始處理向server送出猜字串的udp封包部分
         self.guess_count += 1
         try:
             self.socket.sendto(f"[Guess]: {guess}".encode(), self.server_address)
-            self.modify_output_text(f"[Info]: {self.server_address}已送出猜測({guess})\n")
+            self.modify_output_text(f"[Info]: {self.server_address}已送出猜測({guess})\n", "info")
         except Exception as e:
-            self.modify_output_text(f"[Error]: >{self.server_address[0]}:{self.server_address[1]}，傳送猜字串封包失敗失敗({e})\n")
+            self.modify_output_text(f"[Error]: >{self.server_address[0]}:{self.server_address[1]}，傳送猜字串封包失敗失敗({e})\n", "error")
     
     def receive_response(self):
-        while True:
+        while self.game_running:
             try:
-                data, _ = self.socket.recvfrom(1024)
+                if self.socket:
+                    data, _ = self.socket.recvfrom(1024)
                 response = data.decode()
+                self.reset_timeout_timer()
                 
                 # 檢查有無收到server送來的答案已設定好之Ready封包，接下來才可開始猜字串
                 if response.startswith("[Ready]:"):
                     self.ready_to_guess = True # 設定遊戲已開始，方便後續遊戲狀態判斷
                     if "[Ready]:" in response:
                         self.answer_length = int(response.split(":")[1].split("，")[0]) # 從server發送的ready訊息得到答案n為多少
-                    self.modify_output_text("[Info]: Server已設定好答案，請輸入你猜的字串(0~9, A~F)\n")
+                    self.modify_output_text("[Info]: Server已設定好答案，請輸入你猜的字串(0~9, A~F)\n", "info")
                     self.guess_button.config(state='normal') # 設定猜答案按鈕可以開始用，在設定好答案前為不可用
                     self.start_time = datetime.now() # 設定開始猜的時間
-                    continue            
-                elif response.startswith("[Success]:") and "恭喜猜對" in response:
+                    continue
+                elif response.startswith("[Guess Reply]:") and "恭喜猜對" in response:
                     self.replay_button.config(state="normal") # 設定reaply按鈕猜對後可以再次遊玩
                     duration = (datetime.now() - self.start_time).total_seconds()
                     finish_time_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    self.modify_output_text(f"[Game Finish!]: 共猜{self.guess_count}次，用時{duration:.2f}秒\n")
+                    self.modify_output_text(f"[Game Finish!]: 共猜{self.guess_count}次，用時{duration:.2f}秒\n", "bold")
+                    self.modify_output_text(f"若要再次遊玩請點擊下方「再玩一次」按鈕!", "success")
                     self.guess_button.config(state="disabled")
+                    self.start_button.config(state="normal")
                     # 傳送使用者遊戲資訊回server
-                    userinfo_msg = f"[USERINFO]:{self.username},{self.guess_count},{duration},{finish_time_str}"
+                    userinfo_msg = f"[USERINFO]->{self.username},{self.guess_count},{duration},{finish_time_str}"
                     self.socket.sendto(userinfo_msg.encode(), self.server_address)
-            except Exception as e:
-                self.modify_output_text(f"[Error]: {e}")
+                elif response.startswith("[Guess Reply]"):
+                    self.modify_output_text(f"{response}\n", 'bold')
+                elif response.startswith("[Timeout]:"):
+                    self.modify_output_text(f"{response}", "error")
+                    self.guess_button.config(state="disabled")
+                    self.replay_button.config(state="disabled")
+                    self.game_running = False
+                    self.start_button.config(state="normal")
+                    self.modify_output_text(f"[Info]: 客戶端socket已關閉\n", "info")
+                    self.socket.close()
+            except OSError:
                 break
-            self.modify_output_text(f"[Server Reply]: {response}\n")
-
+            except Exception as e:
+                # self.modify_output_text(f"[Error]: {e}\n", "error")
+                continue
+            # self.modify_output_text(f"[Server Reply]: {response}\n")
     
-    def modify_output_text(self, text):
+    def modify_output_text(self, text, tag=None):
         self.output_text.config(state="normal")
-        self.output_text.insert(tk.END, text)
+        if tag:
+            self.output_text.insert(tk.END, text, tag)
+        else:
+            self.output_text.insert(tk.END, text)
         self.output_text.config(state="disabled")
         self.output_text.see(tk.END)
+
+    # 當有動作時，重製timeout timer
+    def reset_timeout_timer(self):
+        # 由於是threading，需要先把舊的thread給取消掉再開新的timer
+        if self.timeout_timer:
+            self.timeout_timer.cancel()
+        self.timeout_timer = threading.Timer(self.TIMEOUT_DURATION, self.handle_timeout)
+        self.timeout_timer.start()
+        
+    def handle_timeout(self):
+        self.modify_output_text(f"[Timeout]: {self.TIMEOUT_DURATION}秒內沒有互動，遊戲已自動結束!\n", "error")
+        self.game_running = False
+        print(self.server_address)
+        # 同時也中斷server socket的監聽
+        try:
+            self.socket.sendto("[Timeout]: Client已閒置過久，自動中止遊戲".encode(), self.server_address)
+        except Exception as e:
+            self.modify_output_text(f"[Error]: 傳送timeout通知給server失敗: {e}\n", "error")
+        # 關閉socket並解除綁定
+        if self.socket:
+            try:
+                self.socket.close()
+                self.modify_output_text("[Info]: 客戶端socket已關閉\n", "info")
+            except Exception as e:
+                self.modify_output_text(f"[Error]: 關閉socket時發生錯誤：{e}\n", "error")
+            finally:
+                self.socket = None
+        # 重製各個參數
+        self.game_running = False
+        self.answer_length = 0
+        self.server_address = None
+        # 調整按鈕設定
+        self.start_button.config(state="normal")
+        self.guess_button.config(state="disabled")
+        self.replay_button.config(state="disabled")            
 
 # 啟動 Client GUI
 if __name__ == "__main__":
--- server_GUI.py
+++ server_GUI.py
@@ -16,12 +16,17 @@ class ServerGUI:
         # socket變數
         self.server_socket = None # 用於存放server socket的實體
         self.client_address =None
-        self.running = False
         self.receive_thread = None
 
         # answer變數
         self.answer = "" # 紀錄目前設定的答案，用於後續猜的時候比對
         self.answer_length = 0 # 紀錄目前設定的答案長度，同樣用於後續猜的時候比對
+        self.game_running = False
+
+        # timeout相關參數
+        self.timeout_timer = None
+        self.TIMEOUT_DURATION = 120  # 2分鐘
+        self.socket_running = False
 
         # ranking變數紀錄排名
         self.rankings = None
@@ -49,6 +54,10 @@ class ServerGUI:
         # 用於監控的訊息輸出框
         self.output_text = tk.Text(self.text_frame, state="disabled")
         self.output_text.grid(row=0, column=0, sticky="nsew")
+        self.output_text.tag_config("bold", font=("Helvetica", 10, "bold"))
+        self.output_text.tag_config("error", foreground="red")
+        self.output_text.tag_config("success", foreground="green")
+        self.output_text.tag_config("info", foreground="blue", font=("Helvetica", 10, "bold"))
         # 加上scrollbar，可以調整訊息框閱讀範圍
         scrollbar_x = tk.Scrollbar(self.text_frame, orient="horizontal", command=self.output_text.xview)
         scrollbar_y = tk.Scrollbar(self.text_frame, orient='vertical', command=self.output_text.yview)
@@ -84,27 +93,29 @@ class ServerGUI:
         port = self.port_entry.get()
 
         if not ip or not port:
-            self.modify_output_text("[Error]: 輸入錯誤，請輸入IP和Port\n")
+            self.modify_output_text("[Error]: 輸入錯誤，請輸入IP和Port\n", "error")
             return
         
         try:
             port = int(port)
         except ValueError:
-            self.modify_output_text("[Error]: 請確認Port輸入為數字\n")
+            self.modify_output_text("[Error]: 請確認Port輸入為數字\n", "error")
             return
 
         try:
             self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.server_socket.bind(ip, port)
+            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+            self.server_socket.bind((ip, port))
         except Exception as e:
-            self.modify_output_text(f"[Error]: Socket 綁定失敗：{e}\n")
+            self.modify_output_text(f"[Error]: Socket 綁定失敗：{e}\n", "error")
             return
 
-        self.running = True
+        self.socket_running = True
         self.set_answer_button.config(state="normal")
-        self.modify_output_text(f"[Info]: 伺服器已啟動，監聽 {ip}:{port}\n")
+        self.modify_output_text(f"[Info]: 伺服器已啟動，監聽 {ip}:{port}\n", "info")
         self.receive_thread = threading.Thread(target=self.receive_messages, daemon=True)
         self.receive_thread.start()
+        self.reset_timeout_timer() # reset timeout 
 
     def check_client_guess(self, guess: str):
         # 若猜測位數不正確，則直接結束後續判斷，並回應告知client該問題
@@ -114,22 +125,27 @@ class ServerGUI:
         A = sum(a == b for a, b in zip(guess, self.answer))
         B = sum(min(guess.count(x), self.answer.count(x)) for x in set(guess)) - A
         if A == self.answer_length: # 猜到正確答案回傳答對的封包給client
-            return f"[Success]: 恭喜猜對了!{A}A{B}B"
+            return f"恭喜猜對了!{A}A{B}B"
         return f"{A}A{B}B"
 
     def receive_messages(self):
-        while self.running:
+        while self.socket_running:
             try:
-                data, addr = self.server_socket.recvfrom(1024)
+                if self.server_socket:
+                    data, addr = self.server_socket.recvfrom(1024)
                 self.client_address = addr
-                print(data)        
+                self.reset_timeout_timer()
+
                 msg = data.decode()
                 self.modify_output_text(f"[UDP]: 來自 {addr} 的訊息：{msg}\n")
                 
                 # 第一次client向server發送封包確認
                 if msg.startswith("[Connecting]:"):
-                    self.modify_output_text(f"[Success]: 收到來自{addr}的連接訊息\n")
-                    self.client_address = addr  # 確保儲存位址                
+                    self.modify_output_text(f"[Success]: 收到來自{addr}的連接訊息\n", "success")
+                    self.client_address = addr  # 確保儲存位址
+                    self.game_running = True
+                    self.set_answer_button.config(state='normal')
+                    self.server_socket.sendto("[Ack]: Server已啟動".encode(), addr)       
                 # 當client發送packet為guess相關時，進行答案判斷
                 elif msg.startswith("[Guess]:"):
                     if not self.answer: # 防止若server還未設定好答案client就開始猜
@@ -138,11 +154,12 @@ class ServerGUI:
                         guess = msg.split(":")[1].strip()
                         server_reply = self.check_client_guess(guess) # 檢查猜測和答案，回應幾A幾B
                     
-                    self.modify_output_text(f"[Info]: 來自{addr}的猜測：{guess}→{server_reply}\n")
-                    self.server_socket.sendto(server_reply.encode(), self.client_address)
+                    self.modify_output_text(f"[Info]: 來自{addr}的猜測：{guess}→{server_reply}\n", "info")
+                    
+                    self.server_socket.sendto(f"[Guess Reply]: {server_reply}".encode(), self.client_address)
                 # 猜對後，處理client遊戲資料:
-                elif msg.startswith("[USERINFO]:"):
-                    data = msg.split(":")[1].split(",")
+                elif msg.startswith("[USERINFO]"):
+                    data = msg.split("->")[1].split(",")
                     username = data[0]
                     guess_count = int(data[1])
                     duration = float(data[2])
@@ -153,30 +170,52 @@ class ServerGUI:
                     self.server_socket.sendto(f"[Congratulations!]: {username}！你是第 {rank}名!\n".encode(), self.client_address)
                     # server端show出目前ranking狀態
                     self.show_rankings(highlight_username=username, highlight_time=duration, highlight_finish=finish_time_str)
-                elif msg.startswith("[REPLAY]:"):
-                    self.modify_output_text(f"[Info]: client端要求重新開始遊戲，請再次設定答案\n")
+                elif msg.startswith("[Replay]:"):
+                    self.modify_output_text(f"[Info]: client端要求重新開始遊戲，請再次設定答案\n", "info")
                     self.answer = ""
                     self.answer_length = 0
+                    self.set_answer_button.config(state='normal')
                     self.client_address = addr
-
-
+                elif msg.startswith("[Timeout]:"):
+                    self.modify_output_text(f"{msg}\n", "error")
+                    self.answer = ""
+                    self.answer_length = 0
+                    self.client_address = None
+                    self.socket_running = False
+                    self.game_running = False
+                    self.set_answer_button.config(state="normal")
+                    self.modify_output_text(f"[Info]: 伺服器socket已關閉\n", "info")
+                    self.server_socket.close()
+            except OSError:
+                break  # socket 關閉，跳出迴圈結束 thread
             except Exception as e:
-                self.modify_output_text(f"[Error]: {e}")
+                self.modify_output_text(f"[Error]: {e}", "error")
                 continue
+        
+        if not self.socket_running:
+            self.server_socket.close()
+            self.server_socket = None
     
     def stop_server(self):
-        self.running = False
+        self.socket_running = False
+        self.game_running = False
         if self.server_socket:
             self.server_socket.close()
-            self.output_text.insert(tk.END, "[Info]: 伺服器已關閉。\n")
+        self.server_socket = None
 
         root.destroy()
         # 開新 thread 來接收 client 訊息
         # threading.Thread(target=self.receive_messages, daemon=True).start()
 
     def set_answer(self):
+        self.reset_timeout_timer()
         answer = self.answer_entry.get().upper()
         length = 0
+        
+        if not self.game_running:
+            self.modify_output_text("[Info]: 目前無已連接之client，請再次確認是否已有連線\n", "info")
+            self.set_answer_button.config(state="disabled")
+            return
         
         try:
             length = int(self.answer_len_entry.get())
@@ -186,28 +225,33 @@ class ServerGUI:
         
         allowed_input_char = set("0123456789ABCDEF")
         if len(answer) != length:
-            self.modify_output_text(f"[Error]: 答案長度應為 {length} 位\n")
+            self.modify_output_text(f"[Error]: 答案長度應為 {length} 位\n", "error")
             return
         if any(c not in allowed_input_char for c in answer):
-            self.modify_output_text("[Error]: 請確認答案僅包含0~9或A~F\n")
+            self.modify_output_text("[Error]: 請確認答案僅包含0~9或A~F\n", "error")
             return
         if len(set(answer)) != length:
-            self.modify_output_text("[Error]: 答案中有重複字元)\n")
+            self.modify_output_text("[Error]: 答案中有重複字元)\n", "error")
             return
         
         self.answer = answer
         self.answer_length = length
-        self.modify_output_text(f"[Info]: ✅正確答案已設定為：{answer}\n")
+        self.set_answer_button.config(state='disabled')
+        self.modify_output_text(f"[Success]: ✅正確答案已設定為：{answer}\n", "success")
 
         # 若答案設定完成，傳送ready的UDP封包告知client可以開始進行猜字串遊戲
         if self.client_address:
             self.server_socket.sendto(f"[Ready]: {length}，開始猜數字遊戲，請輸入{length}個數字/文字".encode(), self.client_address)
-        print(self.answer_length)
+            print(self.client_address)
 
-    def modify_output_text(self, text):
+    def modify_output_text(self, text, tag=None):
         self.output_text.config(state="normal")
-        self.output_text.insert(tk.END, text)
+        if tag:
+            self.output_text.insert(tk.END, text, tag)
+        else:
+            self.output_text.insert(tk.END, text)
         self.output_text.config(state="disabled")
+        self.output_text.see(tk.END)
         
     def load_rankings(self):
         if os.path.exists(RANKING_FILE):
@@ -231,13 +275,8 @@ class ServerGUI:
             "finish_time": finish_time_str
         })
         self.rankings.sort(key=lambda x: x["time"])
-        self.save_rankings()
-        
-        self.modify_output_text("[Ranking]: \n")
-        for i, record in enumerate(self.rankings, 1):
-            self.modify_output_text(
-                f"{i}. {record['name']} - {record['guesses']} 次, {record['time']} 秒\n"
-            )
+        self.save_rankings() 
+
     # 用來獲取目前遊戲使用者為第幾名
     def get_rank(self, username, time_used, finish_time_str):
         for i, rec in enumerate(self.rankings):
@@ -247,7 +286,7 @@ class ServerGUI:
     
     # 參數用以指定要粗體顯示的目前使用者!
     def show_rankings(self, highlight_username=None, highlight_time=None, highlight_finish=None):
-        self.modify_output_text("[Ranking]:\n")
+        self.modify_output_text("[Ranking]:\n", "bold")
 
         for i, rec in enumerate(self.rankings, 1):
             is_highlight = (
@@ -258,11 +297,48 @@ class ServerGUI:
             line = f"{i}. {rec['name']} - {rec['guesses']} 次, {rec['time']} 秒, 完成時間：{rec['finish_time']}\n"
             if is_highlight:
                 self.output_text.config(state="normal")
-                self.output_text.insert(tk.END, line, "bold") #設定為粗體字
+                self.modify_output_text(line, "bold")
                 self.output_text.config(state="disabled")
             else:
                 self.modify_output_text(line)
+    
+    # 當有動作時，重製timeout timer
+    def reset_timeout_timer(self):
+        # 由於是threading，需要先把舊的thread給取消掉再開新的timer
+        print("timeout reset")
+        if self.timeout_timer:
+            self.timeout_timer.cancel()
+        self.timeout_timer = threading.Timer(self.TIMEOUT_DURATION, self.handle_timeout)
+        self.timeout_timer.start()
         
+    def handle_timeout(self):
+        self.modify_output_text(f"[Timeout]: {self.TIMEOUT_DURATION}秒內沒有互動，遊戲已自動結束!\n", "error")
+        # 同時也中斷client端的socket監聽
+        if self.client_address:
+            try:
+                self.server_socket.sendto(f"[Timeout]: Server已閒置過久，自動中止遊戲".encode(), self.client_address)
+            except Exception as e:
+                self.modify_output_text(f"[Error]: 傳送timeout通知失敗: {e}\n", "error")
+            finally:
+                self.client_address = None
+        # 關閉socket並解除綁定
+        if self.server_socket:
+            try:
+                self.server_socket.close()
+                self.modify_output_text("[Info]: 伺服器socket已關閉\n", "info")
+            except Exception as e:
+                self.modify_output_text(f"[Error]: 關閉socket時發生錯誤：{e}\n", "error")
+            finally:
+                self.server_socket = None
+        
+        # 重製各個參數
+        self.socket_running = False
+        self.game_running = False
+        self.answer = ""
+        self.answer_length = 0
+        
+        # 將設定答案按鈕設為 disabled
+        self.set_answer_button.config(state="disabled")
 
 if __name__ == "__main__":
     root = tk.Tk()
